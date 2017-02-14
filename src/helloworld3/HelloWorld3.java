c4754e094bdc62f0a744221aaa763fc8faa83764
qiwan7383 <qiwan7383@7KSM6Y1.Student.UGDSB.ED>
2017-02-14 13:21:54 -0500

    first

---
+++ src/helloworld3/HelloWorld3.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package helloworld3;
+
+import static java.time.Clock.system;
+
+/**
+ *
+ * @author qiwan7383
+ */
+public class HelloWorld3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("HelloWord3");
+    }
+    
+}
